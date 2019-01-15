526d83f69534568c6a917f946d421850690962a8
Milan Košťák <milankostak@seznam.cz>
2019-01-15 17:15:24 +0100

    RENAME menu -> menu_main

--- app/src/main/java/cz/milan_kostak/altitude/MainActivity.kt
+++ app/src/main/java/cz/milan_kostak/altitude/MainActivity.kt
@@ -114,7 +114,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 

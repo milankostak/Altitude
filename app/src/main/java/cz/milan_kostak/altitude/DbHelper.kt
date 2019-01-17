044efd8c5cf3d156f9bd10da5b0de281716f08a0
Milan Košťák <milankostak@seznam.cz>
2019-01-18 00:14:43 +0100

    ADD DbHelper KDoc

--- app/src/main/java/cz/milan_kostak/altitude/DbHelper.kt
+++ app/src/main/java/cz/milan_kostak/altitude/DbHelper.kt
@@ -9,16 +9,29 @@ import cz.milan_kostak.altitude.model.LocationItem_Table
 
 object DbHelper {
 
+    /**
+     * Get one item by location ID
+     * @return LocationItem?
+     */
     @JvmStatic
     fun getItemById(locationId: Int): LocationItem? {
         return SQLite.select().from(LocationItem::class.java).where(LocationItem_Table.id.eq(locationId)).querySingle()
     }
 
+    /**
+     * Get all locations sorted by time ascending
+     * @return mutable list of location items
+     */
     @JvmStatic
     fun getAllItems(): MutableList<LocationItem> {
         return getAll().orderBy(LocationItem_Table.time, true).queryList()
     }
 
+    /**
+     * Get all locations sorted by given parameter
+     * @param[sortType] tells how should locations be ordered
+     * @return mutable list of location items
+     */
     @JvmStatic
     fun getAllItems(sortType: ListActivity.SortType): MutableList<LocationItem> {
         return when (sortType) {
@@ -33,6 +46,10 @@ object DbHelper {
         return SQLite.select().from(LocationItem::class.java)
     }
 
+    /**
+     * Save all locations
+     * @param[locations] list of locations to be saved
+     */
     @JvmStatic
     fun import(locations: List<LocationItem>?) {
         if (locations != null) {
